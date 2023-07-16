9c1cc3938c84c1562cf5e281cccd23521ad06507
Pawara Gunawardena <pawara.15@cse.mrt.ac.lk>
2023-07-16 14:50:44 +0530

    update query to return reading materials

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -6,6 +6,8 @@ public class Query
     public List<Book> Books => ReadBooks();   
     public List<Magazine> Magazines => ReadMagazines();
 
+    public List<IReadingMaterial> ReadingMaterials => GetReadingMaterials();
+
     private List<Book> ReadBooks()  {
         string fileName = "Database/books.json";
         string jsonString = File.ReadAllText(fileName);
@@ -16,5 +18,12 @@ public class Query
         string fileName = "Database/magazines.json";
         string jsonString = File.ReadAllText(fileName);
         return JsonSerializer.Deserialize<List<Magazine>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
+    }
+
+    
+    private List<IReadingMaterial> GetReadingMaterials()  {
+        var materials = ReadBooks().Cast<IReadingMaterial>().ToList();
+        materials.AddRange(ReadMagazines().Cast<IReadingMaterial>().ToList());
+        return materials;
     }
 }
