2f98f66775a0579f91dbf8750f9afde95f4818a5
Pawara Gunawardena <pawara.15@cse.mrt.ac.lk>
2023-07-16 11:47:45 +0530

    add price and published date to book model
    book object update with price and published date fields

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,4 +2,8 @@ public class Book  {
     public int BookId { get; set; }
     public string Name { get; set; }
     public int Pages { get; set; }
+
+    public double Price {get; set;}
+
+    public DateTime PublishedDate {get; set;}
 }
