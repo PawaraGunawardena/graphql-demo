622f2ade3d9922dbf8212c66b5bc064530db33b8
Pawara Gunawardena <pawara.15@cse.mrt.ac.lk>
2023-07-16 14:49:06 +0530

    add magazines as another reading material
    magazines property update as a field to query from graphql

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -4,10 +4,17 @@ using System.Text.Json.Serialization;
 public class Query
 {
     public List<Book> Books => ReadBooks();   
+    public List<Magazine> Magazines => ReadMagazines();
 
     private List<Book> ReadBooks()  {
         string fileName = "Database/books.json";
         string jsonString = File.ReadAllText(fileName);
         return JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
+    }
+
+    private List<Magazine> ReadMagazines()  {
+        string fileName = "Database/magazines.json";
+        string jsonString = File.ReadAllText(fileName);
+        return JsonSerializer.Deserialize<List<Magazine>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
     }
 }
---
+++ Models/Magazine.cs
@@ -0,0 +1,5 @@
+public class Magazine:  IReadingMaterial {
+    public int IssueNo { get; set; }
+    public string Name { get; set; }
+    public BookGenre Genre {get; set;}
+}
